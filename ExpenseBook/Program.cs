5a00a84fc2d4ee7ed3d5af21fb860b23c7d3f667
Ivanov Ivan <namenastua@gmail.com>
2026-09-11 12:03:09 +0300

    Добавить выбор хранилища параметром запуска

--- ExpenseBook/Program.cs
+++ ExpenseBook/Program.cs
@@ -1,10 +1,34 @@
-﻿using ExpenseBook.Data;
+﻿
 using ExpenseBook.Logic;
+using ExpenseBook.Data;
 
-IExpenseRepository repository = new ExpenseBookRepository();
+
+string jsonPath = Path.Combine(AppContext.BaseDirectory,
+"expenses.json");
+string xmlPath = Path.Combine(AppContext.BaseDirectory,
+"expenses.xml");
+
+string kind = args.Length > 0 ? args[0] : "json";
+
+IExpenseRepository repository = kind switch
+{
+    "xml" => new XmlExpenseRepository(xmlPath),
+    "memory" => new ExpenseBookRepository(),
+    _ => new JsonExpenseRepository(jsonPath)
+};
+Console.WriteLine($"Хранилище: {kind}");
 var service = new ExpenseBookService(repository);
+
+
+Console.Write("Тема новой категории: ");
+string category = Console.ReadLine() ?? "";
+
+service.AddExpense(category, 3200);
 Console.WriteLine("Отобранные записи:");
+
 foreach (var item in service.GetImportant())
 {
     Console.WriteLine($"{item.Id}: {item.Category} - {item.Amount}");
 }
+
+Console.WriteLine($"Сумма расходов по заданной категории: {service.GetTotalAmountByCategory("прооооо")}");
